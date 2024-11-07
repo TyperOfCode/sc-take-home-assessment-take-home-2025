10696d72c7638bdc3e85d1b741521fa326affa27
Ryan <mcryan005@gmail.com>
2024-11-08 00:10:25 +1100

    feat: changed getFoldersByOrgId to work with map tree structure.

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -9,16 +9,17 @@ func GetAllFolders() []Folder {
 }
 
 func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
-	// folders := f.folders
 
-	// res := []Folder{}
-	// for _, f := range folders {
-	// 	if f.OrgId == orgID {
-	// 		res = append(res, f)
-	// 	}
-	// }
+	res := []Folder{}
+	for _, v := range f.nameToNode {
+		f := v.Folder
 
-	return []Folder{}
+		if f.OrgId == orgID {
+			res = append(res, *f)
+		}
+	}
+
+	return res
 
 }
 
